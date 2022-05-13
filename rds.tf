7e7658d2e2d7f45a43af8bd32a81737ac567deb1
SheyNjila2021 <SheyNjila.devtest@gmail.com>
2022-05-13 11:21:11 -0400

    after token correction

--- rds.tf
+++ rds.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "mydb"
+  db_name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
